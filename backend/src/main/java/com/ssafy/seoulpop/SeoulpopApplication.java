34e3719946d9a57bee088cf996b580b6fff5964c
takealook97 <takealook97@naver.com>
2024-04-22 16:40:44 +0900

    chore: build init

---
+++ backend/src/main/java/com/ssafy/seoulpop/SeoulpopApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.seoulpop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulpopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulpopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/seoulpop/SeoulpopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.seoulpop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulpopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
