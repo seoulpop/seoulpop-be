de0b3b9342e07fef8e8d57975a7e1139c712884e
takealook97 <takealook97@naver.com>
2024-04-23 14:44:21 +0900

    chore: add SwaggerConfig

---
+++ backend/src/main/java/com/ssafy/seoulpop/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.ssafy.seoulpop.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.RequiredArgsConstructor;
+
+@OpenAPIDefinition(
+	info = @Info(title = "SEOUL-POP (서울팝)",
+		description = "SEOUL-POP api document",
+		version = "v1"),
+	servers = {@Server(url = "https://api.tempdeploy.site", description = "Default Server URL")})
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public GroupedOpenApi chatOpenApi() {
+		String[] paths = {"/v1/**", "/ping"};
+
+		return GroupedOpenApi.builder()
+			.group("SEOUL-POP API v1")
+			.pathsToMatch(paths)
+			.build();
+	}
+}
