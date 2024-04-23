522d9153a49188257076c791d17669761a33475e
takealook97 <takealook97@naver.com>
2024-04-23 13:23:01 +0900

    chore: add HealthCheckController

---
+++ backend/src/main/java/com/ssafy/seoulpop/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.ssafy.seoulpop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @Tag(name = "서버 상태 컨트롤러", description = "핑 테스트로 서버 상태를 확인하는 기능이 포함되어 있음")
+@RestController
+@RequestMapping
+public class HealthCheckController {
+	// @Operation(summary = "서버 상태 확인(핑 테스트)")
+	@GetMapping("/ping")
+	public String ping() {
+		return "pong";
+	}
+}
