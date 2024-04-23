41e83ef410fda362df102673f3cb63e95bdf6500
takealook97 <takealook97@naver.com>
2024-04-23 14:44:48 +0900

    chore: set CorsConfig and WebConfig

---
+++ backend/src/main/java/com/ssafy/seoulpop/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.ssafy.seoulpop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			// .allowedOrigins("https://tempdeploy.site")
+			// .allowedOrigins("https://localhost:3000")
+			.allowedOriginPatterns("*")
+			.allowedMethods(
+				HttpMethod.GET.name(),
+				HttpMethod.POST.name(),
+				HttpMethod.PUT.name(),
+				HttpMethod.DELETE.name()
+			)
+			.exposedHeaders("*");
+	}
+}
