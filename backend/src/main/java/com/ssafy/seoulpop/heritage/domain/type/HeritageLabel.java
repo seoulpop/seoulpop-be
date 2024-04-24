aa8e38620b4a904640818d46bb081ea3c51df73b
takealook97 <takealook97@naver.com>
2024-04-24 14:39:43 +0900

    feat: add constant enum class

---
+++ backend/src/main/java/com/ssafy/seoulpop/heritage/domain/type/HeritageLabel.java
@@ -0,0 +1,9 @@
+package com.ssafy.seoulpop.heritage.domain.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum HeritageLabel {
+}
---
+++ backend/src/main/java/com/ssafy/seoulpop/site/domain/type/SiteLabel.java
@@ -0,0 +1,9 @@
+package com.ssafy.seoulpop.site.domain.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SiteLabel {
+}
