c816eef3e4efaa8f34e085a72fe087bc511670c2
takealook97 <takealook97@naver.com>
2024-04-24 14:43:12 +0900

    feat: add Heritage Image Entity class

---
+++ backend/src/main/java/com/ssafy/seoulpop/heritage/image/HeritageImage.java
@@ -0,0 +1,35 @@
+package com.ssafy.seoulpop.heritage.image;
+
+import com.ssafy.seoulpop.common.BaseEntity;
+import com.ssafy.seoulpop.heritage.domain.Heritage;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class HeritageImage extends BaseEntity {
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "heritage_id")
+	private Heritage heritage;
+
+	@NonNull
+	@Lob
+	private String imageUrl;
+
+	@NonNull
+	private String caption;
+}
