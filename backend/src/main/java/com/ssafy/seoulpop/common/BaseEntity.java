dbb8bc3cd6f849a13b4192edfe9a84c46fb00cb0
takealook97 <takealook97@naver.com>
2024-04-24 14:41:29 +0900

    feat: add Base Entity

---
+++ backend/src/main/java/com/ssafy/seoulpop/common/BaseEntity.java
@@ -0,0 +1,38 @@
+package com.ssafy.seoulpop.common;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@CreatedDate
+	@Column(columnDefinition = "datetime(0) default now(0)", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(columnDefinition = "datetime(0) default now(0)", nullable = false)
+	private LocalDateTime updatedAt;
+}
