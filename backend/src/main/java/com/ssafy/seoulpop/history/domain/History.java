baad3d5b86c30b947b86af9d5a7f0a7c75035873
takealook97 <takealook97@naver.com>
2024-04-24 14:42:09 +0900

    feat: add History Entity class

---
+++ backend/src/main/java/com/ssafy/seoulpop/history/domain/History.java
@@ -0,0 +1,31 @@
+package com.ssafy.seoulpop.history.domain;
+
+import com.ssafy.seoulpop.common.BaseEntity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class History extends BaseEntity {
+	@NonNull
+	private Double lat;
+
+	@NonNull
+	private Double lng;
+
+	@NonNull
+	private String name;
+
+	@NonNull
+	private String category;
+}
