20db30cd3367d0f6952d5a278c42e8325662ffd3
rglley <foofoo964@naver.com>
2024-04-25 15:33:21 +0900

    feat: add BaseException class

---
+++ backend/src/main/java/com/ssafy/seoulpop/exception/BaseException.java
@@ -0,0 +1,11 @@
+package com.ssafy.seoulpop.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class BaseException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+}
