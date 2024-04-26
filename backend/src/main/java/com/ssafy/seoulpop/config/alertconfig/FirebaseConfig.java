9426793ea45e8636c646c623ed886729f21e465b
rglley <foofoo964@naver.com>
2024-04-26 17:17:39 +0900

    chore: add FirebaseConfig class (#S10P31A409-198)

---
+++ backend/src/main/java/com/ssafy/seoulpop/config/alertconfig/FirebaseConfig.java
@@ -0,0 +1,37 @@
+package com.ssafy.seoulpop.config.alertconfig;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import jakarta.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Autowired
+    private ServiceAccountKey serviceAccountKey;
+
+    @PostConstruct
+    public void initFirebase() throws IOException {
+        Gson gson = new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .setPrettyPrinting()
+            .create();
+        String jsonString = gson.toJson(serviceAccountKey);
+        InputStream serviceAccount = new ByteArrayInputStream(jsonString.getBytes());
+
+        FirebaseOptions options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
