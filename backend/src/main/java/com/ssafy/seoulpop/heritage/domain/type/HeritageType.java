9048bb0bd6a8e49e512e827fc352737c9166a3c1
takealook97 <takealook97@naver.com>
2024-04-29 16:13:54 +0900

    remove: delete Heritage and Site domain
    unified architecture to History at once (#S10P31A409-300)

--- backend/src/main/java/com/ssafy/seoulpop/heritage/domain/type/HeritageType.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.seoulpop.heritage.domain.type;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public enum HeritageType {
-}
--- backend/src/main/java/com/ssafy/seoulpop/site/domain/type/SiteType.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.seoulpop.site.domain.type;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public enum SiteType {
-}
