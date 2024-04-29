b95ea97cc854869c7e1eb4332815f84ebf9699e0
takealook97 <takealook97@naver.com>
2024-04-29 16:31:50 +0900

    feat: add HeritageLabel and HeritageType enum (#S10P31A409-300)

--- backend/src/main/java/com/ssafy/seoulpop/history/domain/type/HeritageLabel.java
+++ backend/src/main/java/com/ssafy/seoulpop/history/domain/type/HeritageLabel.java
@@ -1,9 +1,10 @@
-package com.ssafy.seoulpop.history.type;
+package com.ssafy.seoulpop.history.domain.type;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum SiteLabel {
+public enum HeritageLabel {
+
 }
---
+++ backend/src/main/java/com/ssafy/seoulpop/history/domain/type/SiteLabel.java
@@ -0,0 +1,9 @@
+package com.ssafy.seoulpop.history.domain.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SiteLabel {
+}
