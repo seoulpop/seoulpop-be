a14d7daae3d6d98c986a6743f858a8d241f4c3ec
rglley <foofoo964@naver.com>
2024-04-30 12:24:53 +0900

    chore: add Enums (#S10P31A409-204)

--- backend/src/main/java/com/ssafy/seoulpop/history/domain/type/HistoryCategory.java
+++ backend/src/main/java/com/ssafy/seoulpop/history/domain/type/HistoryCategory.java
@@ -6,4 +6,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum HistoryCategory {
+
+    HERITAGE("문화재"),
+    LIBERATION_MOVEMENT("3·1운동"),
+    KOREAN_WAR("6·25 전쟁");
+
+    private final String description;
 }
