5e6d57d7652d9d00b84c7f9c277964656876731d
rglley <foofoo964@naver.com>
2024-04-30 15:10:42 +0900

    refactor: 도감 등록 파라미터 body로 받아서 처리(AtlasRegistRequestDto) (#S10P31A409-204)

--- backend/src/main/java/com/ssafy/seoulpop/atlas/controller/AtlasController.java
+++ backend/src/main/java/com/ssafy/seoulpop/atlas/controller/AtlasController.java
@@ -1,6 +1,7 @@
 package com.ssafy.seoulpop.atlas.controller;
 
 import com.ssafy.seoulpop.atlas.dto.AtlasInfoResponseDto;
+import com.ssafy.seoulpop.atlas.dto.AtlasRegistRequestDto;
 import com.ssafy.seoulpop.atlas.service.AtlasService;
 import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,11 +32,11 @@ public class AtlasController {
 
     @Operation(
         summary = "도감 등록",
-        description = "AR 인식이 완료된 역사를 도감에 등록합니다(RequestParam: 회원 아이디(임시), 역사 아이디)"
+        description = "AR 인식이 완료된 역사를 도감에 등록합니다(RequestBody: 회원 아이디(임시), 역사 아이디)"
     )
     @PostMapping
-    public ResponseEntity<Void> addAtlas(@RequestParam Long memberId, @RequestParam Long historyId) {
-        atlasService.createAtlas(memberId, historyId);
+    public ResponseEntity<Void> addAtlas(@RequestBody AtlasRegistRequestDto requestDto) {
+        atlasService.createAtlas(requestDto.memberId(), requestDto.historyId());
         return ResponseEntity.ok().build();
     }
 }
