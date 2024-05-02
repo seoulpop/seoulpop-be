c5581ae1a692d7f14e541314f96ff255db34ac5e
Jaeseon95 <hadueat@gmail.com>
2024-05-02 10:48:32 +0900

    feat: add and fix query for visited (#S10P31A409-285)

--- backend/src/main/java/com/ssafy/seoulpop/history/repository/HistoryCustomRepository.java
+++ backend/src/main/java/com/ssafy/seoulpop/history/repository/HistoryCustomRepository.java
@@ -1,10 +1,16 @@
 package com.ssafy.seoulpop.history.repository;
 
 import com.ssafy.seoulpop.history.domain.History;
+import com.ssafy.seoulpop.history.dto.HistoryMapResponseDto;
+import com.ssafy.seoulpop.history.dto.NearByHistoryResponseDto;
 import java.util.List;
 
 public interface HistoryCustomRepository {
 
-    List<History> findByCellList(int level, List<String> cellList);
+    List<NearByHistoryResponseDto> findByCellList(Long memberId, int level, List<String> cellList);
+
+    List<HistoryMapResponseDto> findAllByMemberId(Long memberId);
+
+    List<HistoryMapResponseDto> findAllByMemberIdAndCategory(Long memberId, String category);
 
 }
