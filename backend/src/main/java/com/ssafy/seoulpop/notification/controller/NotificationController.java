b52fd37a5f531a912317cdec9f325047a691c2e9
rglley <foofoo964@naver.com>
2024-05-02 16:12:38 +0900

    style: requestmapping 수정 (#S10P31A409-198)

--- backend/src/main/java/com/ssafy/seoulpop/notification/controller/NotificationController.java
+++ backend/src/main/java/com/ssafy/seoulpop/notification/controller/NotificationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/alerts")
+@RequestMapping("v1/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
 
