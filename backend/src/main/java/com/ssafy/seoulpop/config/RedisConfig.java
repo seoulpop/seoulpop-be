fd9896fbf83040ebc8b32d54841fa6eae9affe00
rglley <foofoo964@naver.com>
2024-05-02 15:06:45 +0900

    chore: RedisTemplate 설정 추가 (#S10P31A409-198)

--- backend/src/main/java/com/ssafy/seoulpop/config/RedisConfig.java
+++ backend/src/main/java/com/ssafy/seoulpop/config/RedisConfig.java
@@ -1,9 +1,13 @@
 package com.ssafy.seoulpop.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -19,6 +23,11 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return createLettuceConnectionFactory(0);
+    }
+
     private LettuceConnectionFactory createLettuceConnectionFactory(int database) {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(host);
@@ -30,5 +39,16 @@ public class RedisConfig {
         lettuceConnectionFactory.afterPropertiesSet();
 
         return lettuceConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericToStringSerializer<>(Object.class));
+        return template;
     }
 }
