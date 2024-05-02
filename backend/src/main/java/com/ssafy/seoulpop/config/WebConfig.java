60272eba530ed63acc4dd0e90f48d861bc9d58a0
takealook97 <takealook97@naver.com>
2024-05-02 17:16:51 +0900

    feat: add OauthServerType converter on WebConfig (#S10P31A409-297) (#S10P31A409-293)

--- backend/src/main/java/com/ssafy/seoulpop/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/seoulpop/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.ssafy.seoulpop.config;
 
+import com.ssafy.seoulpop.member.util.OauthServerTypeConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -23,5 +25,10 @@ public class WebConfig implements WebMvcConfigurer {
                 HttpMethod.DELETE.name()
             )
             .exposedHeaders("*");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new OauthServerTypeConverter());
     }
 }
