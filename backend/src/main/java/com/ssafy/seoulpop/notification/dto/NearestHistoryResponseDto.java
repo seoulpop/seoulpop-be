62b82e0f71b6422ef7beafad0399cf3e7a4ffb05
rglley <foofoo964@naver.com>
2024-05-02 15:14:55 +0900

    chore: 가장 가까운 역사 정보 DTO 추가 (#S10P31A409-198)

---
+++ backend/src/main/java/com/ssafy/seoulpop/notification/dto/NearestHistoryResponseDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.seoulpop.notification.dto;
+
+import lombok.Builder;
+
+@Builder
+public record NearestHistoryResponseDto(
+    Long historyId,
+    String name,
+    String category,
+    Integer distance
+) {
+
+}
