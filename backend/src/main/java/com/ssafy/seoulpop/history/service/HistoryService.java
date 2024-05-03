308c4c099f0b6d5f6b2b92309b09babb85ae9945
Jaeseon95 <hadueat@gmail.com>
2024-05-03 16:24:27 +0900

    feat: read AR marker info (#S10P31A409-287)

--- backend/src/main/java/com/ssafy/seoulpop/history/service/HistoryService.java
+++ backend/src/main/java/com/ssafy/seoulpop/history/service/HistoryService.java
@@ -5,6 +5,7 @@ import com.ssafy.seoulpop.exception.ErrorCode;
 import com.ssafy.seoulpop.history.domain.History;
 import com.ssafy.seoulpop.history.dto.HeritageDto;
 import com.ssafy.seoulpop.history.dto.HistoryMapResponseDto;
+import com.ssafy.seoulpop.history.dto.NearByArResponseDto;
 import com.ssafy.seoulpop.history.dto.NearByHistoryResponseDto;
 import com.ssafy.seoulpop.history.dto.SiteDto;
 import com.ssafy.seoulpop.history.repository.HistoryRepository;
@@ -39,21 +40,28 @@ public class HistoryService {
     }
 
     public List<NearByHistoryResponseDto> readNearByHistoryList(Long memberId, double lat, double lng, int level) {
-        // 위 경도에서 셀 Index로 변환
-        String cellIndex = h3.latLngToCellAddress(lat, lng, level);
-        // 주변 셀 List 확보
-        List<String> cellList = new ArrayList<>(h3.gridDisk(cellIndex, 1));
-
-        return historyRepository.findByCellList(memberId, level, cellList);
+        List<String> cellList = getCellList(lat, lng, level);
+        return historyRepository.findByMemberIdAndCellList(memberId, level, cellList);
     }
 
     public Object readHistoryDetail(Long historyId) {
         History historyDetail = historyRepository.findById(historyId)
             .orElseThrow(() -> new BaseException(ErrorCode.HISTORY_NOT_FOUND_ERROR));
-
         if (historyDetail.getCategory().equals("문화재")) {
             return HeritageDto.from(historyDetail);
         }
         return SiteDto.from(historyDetail);
+    }
+
+    public List<NearByArResponseDto> readNearByArMarker(double lat, double lng, int level) {
+        List<String> cellList = getCellList(lat, lng, level);
+        return historyRepository.findByCellList(level, cellList);
+    }
+
+    private List<String> getCellList(double lat, double lng, int level) {
+        // 위 경도에서 셀 Index로 변환
+        String cellIndex = h3.latLngToCellAddress(lat, lng, level);
+        // 주변 셀 List 확보
+        return new ArrayList<>(h3.gridDisk(cellIndex, 1));
     }
 }
