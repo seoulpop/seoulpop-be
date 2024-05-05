4ed169f8ad04aca35de54611b93dcf380d3d7a68
rglley <foofoo964@naver.com>
2024-05-06 06:53:41 +0900

    feat: fcmToken 쿠키 발급 요청 추가

--- backend/src/main/java/com/ssafy/seoulpop/notification/controller/NotificationController.java
+++ backend/src/main/java/com/ssafy/seoulpop/notification/controller/NotificationController.java
@@ -1,13 +1,14 @@
 package com.ssafy.seoulpop.notification.controller;
 
+import com.ssafy.seoulpop.notification.dto.FcmCookieRequestDto;
 import com.ssafy.seoulpop.notification.dto.NotificationRequestDto;
 import com.ssafy.seoulpop.notification.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1/notifications")
@@ -16,8 +17,21 @@ public class NotificationController {
 
     private final NotificationService notificationService;
 
-    @GetMapping
-    public ResponseEntity<String> getAlert(@RequestBody NotificationRequestDto notificationRequestDto) {
-        return ResponseEntity.ok(notificationService.createNotification(notificationRequestDto));
+    @Operation(
+            summary = "쿠키 발급",
+            description = "fcmToken 이름으로 쿠키 발급"
+    )
+    @PostMapping("/regist")
+    public ResponseEntity<String> getCookie(HttpServletResponse response, @RequestBody FcmCookieRequestDto requestDto) {
+        return ResponseEntity.ok(notificationService.createCookie(response, requestDto));
+    }
+
+    @Operation(
+            summary = "알림 확인 및 생성",
+            description = "사용자 위치 기반 알림 확인, 가장 가까운 역사 알림 생성"
+    )
+    @PostMapping
+    public ResponseEntity<String> getNotification(HttpServletRequest request, @RequestBody NotificationRequestDto requestDto) {
+        return ResponseEntity.ok(notificationService.createNotification(request, requestDto));
     }
 }
