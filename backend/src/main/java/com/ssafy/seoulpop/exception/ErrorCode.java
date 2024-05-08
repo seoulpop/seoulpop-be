a9ee33a72b11d45364f485967697c6d50152cf2a
rglley <foofoo964@naver.com>
2024-05-08 13:51:58 +0900

    chore: FCM 토큰 미등록 에러코드 추가 (#S10P31A409-198)

--- backend/src/main/java/com/ssafy/seoulpop/exception/ErrorCode.java
+++ backend/src/main/java/com/ssafy/seoulpop/exception/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
 
     //4xx
     METHOD_ARGUMENT_NOT_VALID(HttpStatus.BAD_REQUEST, "유효성 검사를 통과하지 못했습니다."),
+    FCM_TOKEN_NOT_REGISTERED_ERROR(HttpStatus.BAD_REQUEST, "FCM 토큰이 등록되어있지 않습니다."),
     FCM_TOKEN_NOT_FOUND_ERROR(HttpStatus.BAD_REQUEST, "FCM 토큰을 찾을 수 없습니다."),
     INVALID_LOCATION_ERROR(HttpStatus.BAD_REQUEST, "유효하지 않은 레벨 값입니다."),
     INVALID_LEVEL_ERROR(HttpStatus.BAD_REQUEST, "유효하지 않은 위,경도입니다."),
