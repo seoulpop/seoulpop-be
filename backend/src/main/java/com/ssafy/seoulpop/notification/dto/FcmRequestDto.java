66e164fca9a6b9d0da7bf324fbdf002e0f3ca8e3
rglley <foofoo964@naver.com>
2024-05-14 12:28:51 +0900

    chore: http v1 요청을 위한 메세지 DTO 추가 (#S10P31A409-198)

---
+++ backend/src/main/java/com/ssafy/seoulpop/notification/dto/FcmRequestDto.java
@@ -0,0 +1,37 @@
+package com.ssafy.seoulpop.notification.dto;
+
+import lombok.Builder;
+
+@Builder
+public record FcmRequestDto(
+    Message message
+) {
+
+    @Builder
+    public record Message(
+        String token,
+        Data data,
+        Notification notification
+    ) {
+
+    }
+
+    @Builder
+    public record Data(
+        String historyId,
+        String historyName,
+        String historyCategory,
+        String historyLat,
+        String historyLng
+    ) {
+
+    }
+
+    @Builder
+    public record Notification(
+        String body,
+        String title
+    ) {
+
+    }
+}
