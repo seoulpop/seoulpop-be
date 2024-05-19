af99eab4757a380f487375aeea6b5b157d544e6b
Jaeseon95 <hadueat@gmail.com>
2024-05-19 17:13:19 +0900

    feat: 도감 전체 정보 조회 API

--- backend/src/main/java/com/ssafy/seoulpop/atlas/repository/AtlasRepositoryCustom.java
+++ backend/src/main/java/com/ssafy/seoulpop/atlas/repository/AtlasRepositoryCustom.java
@@ -6,4 +6,6 @@ import java.util.List;
 public interface AtlasRepositoryCustom {
 
     List<AtlasInfoResponseDto> findAtlasInfoByMemberId(Long memberId);
+
+    List<AtlasInfoResponseDto> findAllAtlasInfo();
 }
